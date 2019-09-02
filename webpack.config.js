879c1124f6a054a572f077055f0482984fec136d
Benjamin Gondy <benjamin.gondy+git@gmail.com>
2019-09-02 16:47:00 +0200

    Configure Webpack to build Scss files

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: "web",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  devtool: "source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            }
+          },
+          // Compiles Sass to CSS
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                data: '@import "@/scss/lib/_lib.scss";',
+              }
+            }
+          },
+        ],
+      },
+    ],
+  },
+};
