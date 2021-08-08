d157e744a301b91765e2586f1e01fc387fed2b27
Cédric Bommart <bommartcedric@gmail.com>
2021-08-09 01:53:26 +0200

    Add css-mqpacker webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CSSMQPackerPlugin = require('css-mqpacker-webpack-plugin');
 
 process.env.NODE_ENV = 'production';
 
@@ -64,9 +65,13 @@ module.exports = {
     ],
   },
   optimization: {
+    minimize: true,
     minimizer: [
       `...`, // Extend existing minimizers
       new CssMinimizerPlugin(),
+      new CSSMQPackerPlugin({
+        sort: true,
+      }),
     ],
   },
 };
